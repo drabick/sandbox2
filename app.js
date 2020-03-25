1c6b7ac5351578fd336dff1b32fd710697fc95b4
Darren Rabick <darrenrabick@Darrens-Mini.local>
2020-03-25 12:57:02 -0500

    re-did inital get and cleaned up some code

--- app.js
+++ app.js
@@ -1,62 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
-const handlebars = require('express-handlebars');
-const fetch = require('node-fetch');
-const connectDB = require('./db');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const handlebars = require("express-handlebars");
+const connectDB = require("./db");
+const cr = require("./lib/streetData");
 
 const app = express();
-require('dotenv/config');
+require("dotenv/config");
 
 //Import post routes
-const postsRoute = require('./routes/posts');
+const postsRoute = require("./routes/posts");
 //Import gets routes
-const getsRoute = require('./routes/gets');
+const getsRoute = require("./routes/gets");
+
+let cdata;
 
 //middleware
 app.use(bodyparser.json());
-app.use('/gets', getsRoute);
-app.use('/posts', postsRoute);
+app.use("/gets", getsRoute);
+app.use("/posts", postsRoute);
 
 //Handlebar engine
 //Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
-    layoutsDir: __dirname + '/views/layouts',defaultLayout: 'index',
-    }));
-app.set('view engine', 'handlebars');
-
-//routes
-//app.get('/', (req,res) => {
-//    res.send('We are on home page');    
-//});
-var cdata;
-
-fetch("https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=block like" + " '%25STATE%25'")
-.then(response => {
-    return response.json()
-})
-.then(data => {
-//Work with JSON data here
-console.log(data[0].id) // Grab first Crime ID from first JSON element in Crime array
-cdata = data;
-})
-.catch(err=> {
-// Do something for an error here
-})
+app.engine(
+	"handlebars",
+	handlebars({
+		layoutsDir: __dirname + "/views/layouts",
+		defaultLayout: "index"
+	})
+);
+app.set("view engine", "handlebars");
 
 //load index layout and fill it with main html data into body
-app.get('/', (req, res) => {
-    
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    //res.render('main', {layout : 'index'});
-    
-    res.render('main', {crimes: cdata});
-
-    });
+app.get("/", async (req, res) => {
+	//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+	//res.render('main', {layout : 'index'});
+	try {
+		cdata = await cr.getData(req.query.street);
+	} catch (e) {
+		//this will eventually be handled by your error handling middleware
+		next(e);
+	}
+	res.render("main", { crimes: cdata });
+});
 
 // Connect to database
 connectDB();
-    
+
 // lisgerner
 
 app.listen(3000);
