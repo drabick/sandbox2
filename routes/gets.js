0d65eeba330a7a3366a784b1328f0828ecf6cc46
Darren Rabick <darrenrabick@Darrens-Mini.local>
2020-03-26 15:49:25 -0500

    Added fixed footer

--- routes/gets.js
+++ routes/gets.js
@@ -1,75 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-const cr = require('../lib/streetData')
+const bodyParser = require("body-parser");
+const cr = require("../lib/streetData");
 
 var cdata;
 
 // for parsing application/json
-router.use(bodyParser.json()); 
+router.use(bodyParser.json());
 
 // for parsing application/xwww-
-router.use(bodyParser.urlencoded({ extended: true })); 
+router.use(bodyParser.urlencoded({ extended: true }));
 
-//For handlebar index page
-router.get('/about', (req,res) => {
-    res.send('this is from get routers about');
+// New inital get for crime app
+router.get("/street", async (req, res, next) => {
+	//Get Street to serqch for
+	console.log("Search for steet");
+	console.log(req.query.street);
+
+	try {
+		cdata = await cr.getStreetData(req.query.street);
+	} catch (e) {
+		//this will eventually be handled by your error handling middleware
+		next(e);
+	}
+	res.render("main", { crimes: cdata });
 });
 
-router.get('/user', function(req, res) {
-  //res.send('user' + req.params.id); 
-  //res.send('User Name');   
-  res.render('page1', {layout : 'index'});
-  
-});
-
-
-router.get('/user/name', function(req, res) {
-  console.log(req.query.user);
-  //res.send('user submitted: ' + req.params('user')); 
-  //res.send('User Name');   
-});
-
-
-
-
-
-
-router.post('/street', async (req,res,next) => {
-
-//Get Street to serqch for
-console.log('Search for steet');
-console.log(req.body.street);
-let street = req.body.street;
-
-    try {
-        cdata = await cr.getStreetData(street);
-      } catch (e) {
-        //this will eventually be handled by your error handling middleware
-        next(e) 
-      }
-    res.render('main', {crimes: cdata});
-});
-
-
-
-
-
-
-
-router.get('/street', async (req,res,next) => {
-
-  //Get Street to serqch for
-  console.log('Search for steet');
-  console.log(req.query.street);
-  
-      try {
-          cdata = await cr.getStreetData(req.query.street);
-        } catch (e) {
-          //this will eventually be handled by your error handling middleware
-          next(e) 
-        }
-      res.render('main', {crimes: cdata});
-  });
-  
-  module.exports = router;
+module.exports = router;
